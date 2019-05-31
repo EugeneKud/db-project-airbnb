8f05289769508ab0d1afa21c1f51eb84e7bd9bce
Eugene Kudryavtsev <eugene.kud@gmail.com>
2019-05-31 22:49:33 +0200

    feat: add actual search feature

--- DbProjectAirbnb.Web/Controllers/SearchController.cs
+++ DbProjectAirbnb.Web/Controllers/SearchController.cs
@@ -1,13 +1,23 @@
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 using System.Threading.Tasks;
+using DbProjectAirbnb.Web.Entities;
 using DbProjectAirbnb.Web.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DbProjectAirbnb.Web.Controllers
 {
     public class SearchController : Controller
     {
+        private readonly ModelContext _modelContext;
+
+        public SearchController(ModelContext modelContext)
+        {
+            _modelContext = modelContext;
+        }
+
         public IActionResult Index()
         {
             return View(new TablesToSearchMask());
@@ -20,8 +30,14 @@ namespace DbProjectAirbnb.Web.Controllers
             if (searchMask.AmenityTypes)
             {
                 if (searchMask.ResultData is null) searchMask.ResultData = new Dictionary<string, DataTable>();
-                searchMask.ResultData.Add("AmenityTypes", new DataTable());
+                var searchQuery = _modelContext.AmenityTypes
+                    .Where(e => e.Name.Contains(searchMask.SearchText));
+                searchMask.AmenityTypesResult = await searchQuery
+                    .Take(10)
+                    .ToListAsync();
+                searchMask.AmenityTypesResultCount = await searchQuery.CountAsync();
             }
+
             return View(searchMask);
         }
     }
--- DbProjectAirbnb.Web/Models/TablesToSearchMask.cs
+++ DbProjectAirbnb.Web/Models/TablesToSearchMask.cs
@@ -1,11 +1,13 @@
 using System.Collections.Generic;
 using System.Data;
+using DbProjectAirbnb.Web.Entities;
 
 namespace DbProjectAirbnb.Web.Models
 {
     public class TablesToSearchMask
     {
         public string SearchText { get; set; }
+        
         public bool AmenityTypes { get; set; }
         public bool Availabilities { get; set; }
         public bool BedTypes { get; set; }
@@ -19,11 +21,30 @@ namespace DbProjectAirbnb.Web.Models
         public bool ListingsHaveAmenityTypes { get; set; }
         public bool ListingsHaveBedTypes { get; set; }
         public bool Neighborhoods { get; set; }
-        public bool PredefinedQueries { get; set; }
         public bool PropertyTypes { get; set; }
         public bool RoomTypes { get; set; }
         public bool Users { get; set; }
         public bool UsersReviewListings { get; set; }
+        
+        public ICollection<AmenityType> AmenityTypesResult { get; set; }
+        public int AmenityTypesResultCount { get; set; }
+        public ICollection<Availability> AvailabilitiesResult { get; set; }
+        public ICollection<BedType> BedTypesResult { get; set; }
+        public ICollection<CancellationPolicy> CancellationPoliciesResult { get; set; }
+        public ICollection<City> CitiesResult { get; set; }
+        public ICollection<Country> CountriesResult { get; set; }
+        public ICollection<HostVerification> HostVerificationsResult { get; set; }
+        public ICollection<Host> HostsResult { get; set; }
+        public ICollection<HostHasHostVerification> HostsHaveHostVerificationsResult { get; set; }
+        public ICollection<Listing> ListingsResult { get; set; }
+        public ICollection<ListingHasAmenityType> ListingsHaveAmenityTypesResult { get; set; }
+        public ICollection<ListingHasBedType> ListingsHaveBedTypesResult { get; set; }
+        public ICollection<Neighborhood> NeighborhoodsResult { get; set; }
+        public ICollection<PropertyType> PropertyTypesResult { get; set; }
+        public ICollection<RoomType> RoomTypesResult { get; set; }
+        public ICollection<User> UsersResult { get; set; }
+        public ICollection<UserReviewsListing> UsersReviewListingsResult { get; set; }
+        
         public Dictionary<string, DataTable> ResultData { get; set; }
     }
 }
